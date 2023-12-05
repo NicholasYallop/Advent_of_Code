2f8f4e1f31968a87cda16e1dc56014e0bf42775f
Nick Yallop <n.yallop@wearewattle.com>
2023-12-05 23:54:00 +0000

    day 4 pt 2

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 readPath = Environment.CurrentDirectory + "/data/" + readPath;
 var streamReader = new StreamReader(readPath);
 
-var ans = day4.card_analyzer.part_one(streamReader);
+var ans = day4.card_analyzer.part_two(streamReader);
 
 Console.WriteLine(ans);
--- solvers/day4.cs
+++ solvers/day4.cs
@@ -26,4 +26,47 @@ public static class card_analyzer
         }
         return result;
     }
+
+    public static int part_two(StreamReader sr){
+        var result = 0;
+
+        var card_number = 1;
+        var extra_card_counts = new Dictionary<int, int>(){
+            {1,0}
+        };
+
+        string? line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            var card_count = 1;
+            if (extra_card_counts.TryGetValue(card_number, out var extras)){
+                card_count += extras;
+            }
+            
+            result += card_count;
+
+            var input = line.Split(':')[1].Split('|')
+                            .Select(numbers => numbers.Trim(' ').Split(' ')
+                                        .Where(number => !string.IsNullOrWhiteSpace(number))
+                                        .Select(number_string => int.Parse(number_string))
+                                        .ToList()
+                            ).ToList();
+
+            var winning_numbers = input[0];
+            var my_numbers = input[1];
+            
+            var winners = winning_numbers.Count(winner => my_numbers.Contains(winner));
+            
+            for (int i=card_number ; i<=card_number+winners ; i++){
+                if (extra_card_counts.ContainsKey(i)){
+                    extra_card_counts[i] += card_count;
+                }else{
+                    extra_card_counts.Add(i,card_count);
+                }
+            }
+
+            card_number++;
+        }
+        return result;
+    }
 }
